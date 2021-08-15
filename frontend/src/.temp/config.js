d23443f798ed7b3eadc733b6a7f7695043823034
guusyy <guus.noij@gmail.com>
2021-08-15 19:09:48 +0200

    Create Strapi starter project

---
+++ frontend/src/.temp/config.js
@@ -0,0 +1,8 @@
+export default {
+  "trailingSlash": true,
+  "pathPrefix": "",
+  "titleTemplate": "%s - Strapi Gridsome Blog",
+  "siteUrl": "",
+  "version": "0.7.23",
+  "catchLinks": true
+}
---
+++ frontend/src/.temp/icons.js
@@ -0,0 +1,41 @@
+export default {
+  "touchiconMimeType": "image/png",
+  "faviconMimeType": "image/png",
+  "precomposed": false,
+  "touchicons": [
+    {
+      "width": 76,
+      "src": "/assets/static/src/favicon.png?width=76&key=b4bc9f7"
+    },
+    {
+      "width": 152,
+      "src": "/assets/static/src/favicon.png?width=152&key=b4bc9f7"
+    },
+    {
+      "width": 120,
+      "src": "/assets/static/src/favicon.png?width=120&key=b4bc9f7"
+    },
+    {
+      "width": 167,
+      "src": "/assets/static/src/favicon.png?width=167&key=b4bc9f7"
+    },
+    {
+      "width": 180,
+      "src": "/assets/static/src/favicon.png?width=180&key=b4bc9f7"
+    }
+  ],
+  "favicons": [
+    {
+      "width": 16,
+      "src": "/assets/static/src/favicon.png?width=16&key=4408023"
+    },
+    {
+      "width": 32,
+      "src": "/assets/static/src/favicon.png?width=32&key=4408023"
+    },
+    {
+      "width": 96,
+      "src": "/assets/static/src/favicon.png?width=96&key=4408023"
+    }
+  ]
+}
