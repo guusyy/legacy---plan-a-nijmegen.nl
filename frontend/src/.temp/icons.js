8dc9ca0445c3d06f26a89aecf0b4c8ea6b610887
guusyy <guus.noij@gmail.com>
2021-08-18 20:34:52 +0200

    changed overlay background transparency

--- frontend/src/.temp/icons.js
+++ frontend/src/.temp/icons.js
@@ -2,40 +2,15 @@ export default {
   "touchiconMimeType": "image/png",
   "faviconMimeType": "image/png",
   "precomposed": false,
-  "touchicons": [
-    {
-      "width": 76,
-      "src": "/assets/static/src/favicon.png?width=76&key=a0a8818"
-    },
-    {
-      "width": 152,
-      "src": "/assets/static/src/favicon.png?width=152&key=a0a8818"
-    },
-    {
-      "width": 120,
-      "src": "/assets/static/src/favicon.png?width=120&key=a0a8818"
-    },
-    {
-      "width": 167,
-      "src": "/assets/static/src/favicon.png?width=167&key=a0a8818"
-    },
-    {
-      "width": 180,
-      "src": "/assets/static/src/favicon.png?width=180&key=a0a8818"
-    }
-  ],
+  "touchicons": [],
   "favicons": [
     {
       "width": 16,
-      "src": "/assets/static/src/favicon.png?width=16&key=06a9a54"
+      "src": "/assets/static/src/favicon.png?width=16&key=8664edc"
     },
     {
       "width": 32,
-      "src": "/assets/static/src/favicon.png?width=32&key=06a9a54"
-    },
-    {
-      "width": 96,
-      "src": "/assets/static/src/favicon.png?width=96&key=06a9a54"
+      "src": "/assets/static/src/favicon.png?width=32&key=8664edc"
     }
   ]
 }
--- frontend/src/.temp/now.js
+++ frontend/src/.temp/now.js
@@ -1,1 +1,1 @@
-export default 1629311200467
+export default 1629311665077
